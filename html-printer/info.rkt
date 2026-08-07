3ee699c048c5e44060c3b7f63af1d75090f6524b
Joel Dueck <joel@jdueck.net>
2026-08-07 17:04:10 -0500

    Declare html-printer-lib as runtime dependency; fixes #8

--- html-printer/info.rkt
+++ html-printer/info.rkt
@@ -3,11 +3,11 @@
 (define implies    '("html-printer-lib"))
 (define build-deps '("racket-doc"
                      "scribble-lib"
-                     "txexpr"
-                     "html-printer-lib"))
+                     "txexpr"))
 (define scribblings '(("scribblings/html-printer.scrbl" ())))
 (define pkg-desc "Business Central REST API client library")
 (define version "0.1")
 (define pkg-authors '(joel))
 (define license '(Apache-2.0 OR MIT))
-(define deps '("base"))
+(define deps '("base"
+               "html-printer-lib"))
